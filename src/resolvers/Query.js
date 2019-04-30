5d6fcfa634545b97e124b805f27f4aeedb1062c2
Ayush Rai <ayush.rai@naukri.com>
2019-04-30 15:23:32 +0530

    adding code to support for pagination and getting count

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,23 @@
-function feed(root, args, context, info) {
-    return context.prisma.links()
+async function feed(root, args, context, info) {
+    const where = args.filter ? {
+        OR: [
+            { description_contains: args.filter },
+            { url_contains: args.filter }
+        ]
+    } : {}
+
+    const links = await context.prisma.links({
+        where,
+        skip: args.skip ? args.skip : null,
+        first: args.first ? args.first : null,
+        orderBy: args.orderBy
+    })
+
+    const count = links.length
+    return {
+        links: links,
+        count: count
+    }
 }
 
 module.exports = {
